7268b9cd6b58aad01225336fa3d74a7cdd3a0edc
wtfranco22 <francofranco_96@hotmail.com>
2023-09-18 01:26:11 -0300

    fix(mobile): se trabaja en responsive

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react"
 import styles from "./Nav.module.css"
 import { Link } from "react-scroll"
 const Nav = () => {
+    const [menu, setMenu] = useState(false);
+    const handleClick = () => { setMenu(!menu) };
     return (
-        <nav className={styles.container}>
-                <Link to="https://www.linkedin.com/in/wtfranco22" target="_blank">
-                    <img className={styles.img} src="/logo.png" alt="logo-wtfranco22" />
-                </Link>
-            <ul className={styles.ul}>
+        <nav className={`${styles.container} ${menu ? styles.open : ''}`}>
+            <Link to="https://www.linkedin.com/in/wtfranco22" target="_blank">
+                <img className={styles.img} src="/logo.png" alt="logo-wtfranco22" />
+            </Link>
+            <div className={styles.mobileMenu} onClick={handleClick}>
+                <div className={`${styles.bar} ${menu ? styles.open : ''}`} />
+                <div className={`${styles.bar} ${menu ? styles.open : ''}`} />
+                <div className={`${styles.bar} ${menu ? styles.open : ''}`} />
+            </div>
+            <ul className={`${styles.ul} ${menu ? styles.show : ''}`}>
                 <li className={styles.li}>
                     <Link
                         to="profile"
@@ -14,7 +22,9 @@ const Nav = () => {
                         smooth={true}
                         offset={-100}
                         duration={700}
-                        className={styles.link}>
+                        className={styles.link}
+                        onClick={handleClick}
+                        >
                         Inicio
                     </Link>
                 </li>
@@ -25,7 +35,9 @@ const Nav = () => {
                         smooth={true}
                         offset={-80}
                         duration={700}
-                        className={styles.link}>
+                        className={styles.link}
+                        onClick={handleClick}
+                        >
                         Proyectos
                     </Link>
                 </li>
@@ -36,7 +48,9 @@ const Nav = () => {
                         smooth={true}
                         offset={-80}
                         duration={700}
-                        className={styles.link}>
+                        className={styles.link}
+                        onClick={handleClick}
+                        >
                         Contacto
                     </Link>
                 </li>
